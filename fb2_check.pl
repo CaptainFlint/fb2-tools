cc5cc566ab50c1db0f287f63522b7e3d82292fe6
Konstantin Vlasov <support@flint-inc.ru>
2016-03-12 14:55:02 +0300

    Rewritten with object test model; added many new tests
    1. Each test is now specified as a separate object, adding new tests is simpler.
    2. Added new tests:
      * file encoding;
      * mismatched Frehcn and English quotes;
      * spaces after closing italic/bold tag;
      * space as first character in the link.

--- fb2_check.pl
+++ fb2_check.pl
@@ -1,13 +1,5 @@
 #!/usr/bin/perl
 
-# Tests performed:
-# 1. Check that all links lead to existing targets.
-# 2. Check that each target has at least one link leading to it
-#    (if not it's not an error, but very suspicios situation).
-
-# TODO:
-# * If there is a link, its target should contain a back-link.
-
 use strict;
 use warnings;
 
@@ -16,17 +8,140 @@ if ((scalar(@ARGV) < 1) || ($ARGV[0] eq '-h') || ($ARGV[0] eq '--help')) {
 	exit 1;
 }
 
-sub inArray($$) {
-	my ($elem, $arr) = @_;
-	for my $e (@$arr) {
-		return 1 if ($e eq $elem);
-	}
-	return 0;
-}
+# TODO:
+# * If there is a link, its target should contain a back-link.
+
+# List of test procedures
+# Each test contains:
+# 'name'    - user-friendly name of the test
+# 'analyze' - function that will be called for each line of the FB2 file;
+#             input: 1) the test object itself, 2) the source line to be analyzed, 3) line number
+# 'report'  - function that will be called after analyzing alal data
+#             input: 1) the test object itself, 2) output handle
+# Other fields may be specified and used internally (e.g. 'data' for gathering problems).
+my @tests = (
+	{
+		# File encoding must be UTF-8.
+		'name' => 'Encoding',
+		'data' => [],
+		'analyze' => sub ($$$) {
+			my ($this, $ln, $idx) = @_;
+			if ($ln =~ m/<\?xml .*encoding="([^\"]+)"/) {
+				if (lc($1) ne 'utf-8') {
+					push @{$this->{'data'}}, "Wrong encoding: '$1'. Expected 'utf-8'.";
+				}
+			}
+		},
+		'report' => sub ($$) {
+			my ($this, $fo) = @_;
+			print $fo join('', map { "\t$_\n" } @{$this->{'data'}});
+		}
+	},
+	{
+		# a) All links lead to existing targets.
+		# b) Each target has at least one link leading to it (may contain false positives:
+		#    orfan ID is not an error, but very suspicios situation).
+		'name' => 'Broken links / Orfan targets',
+		'data' => { 'targets' => {}, 'links' => {} },
+		'analyze' => sub ($$$) {
+			my ($this, $ln, $idx) = @_;
+			$ln =~ s/<[^<>]+ id="([^\"]+)"/$this->{'data'}->{'targets'}->{$1} = 1; $&;/eg;
+			$ln =~ s/<a l:href="#([^\"]+)"/$this->{'data'}->{'links'}->{$1} = 1; $&;/eg;
+		},
+		'report' => sub ($$) {
+			my ($this, $fo) = @_;
+			print $fo "Broken links:\n";
+			for my $elem (sort(keys(%{$this->{'data'}->{'links'}}))) {
+				print $fo "\t$elem\n" if (!$this->{'data'}->{'targets'}->{$elem});
+			}
+			print $fo "Orfan targets:\n";
+			for my $elem (sort(keys(%{$this->{'data'}->{'targets'}}))) {
+				print $fo "\t$elem\n" if (!$this->{'data'}->{'links'}->{$elem});
+			}
+		}
+	},
+	{
+		# Mismatched quotes (may contain false positives when long quotation extends
+		# over several paragraphs).
+		'name' => 'Quotes mismatch',
+		'data' => [],
+		'analyze' => sub ($$$) {
+			my ($this, $ln, $idx) = @_;
+
+			# «French» quotes
+			my $qo = ($ln =~ s/«/$&/g) || 0;
+			my $qc = ($ln =~ s/»/$&/g) || 0;
+			if ($qo != $qc) {
+				push @{$this->{'data'}}, "French quotes mismatch at line $idx: opening - $qo, closing - $qc";
+			}
+			# “English” quotes
+			$qo = ($ln =~ s/“/$&/g) || 0;
+			$qc = ($ln =~ s/”/$&/g) || 0;
+			if ($qo != $qc) {
+				push @{$this->{'data'}}, "English quotes mismatch at line $idx: opening - $qo, closing - $qc";
+			}
+		},
+		'report' => sub ($$) {
+			my ($this, $fo) = @_;
+			print $fo join('', map { "\t$_\n" } @{$this->{'data'}});
+		}
+	},
+	{
+		# There should be no space after closing italic/bold tag if it is at the end of phrase.
+		'name' => 'Spaces after italic/bold',
+		'data' => [],
+		'analyze' => sub ($$$) {
+			my ($this, $ln, $idx) = @_;
+			my $num = ($ln =~ s/<\/(emphasis|strong)>\s([.,?!:;)&\]]|»|”)/$&/g);
+			if ($num) {
+				push @{$this->{'data'}}, "At line $idx, number of entries: $num";
+			}
+		},
+		'report' => sub ($$) {
+			my ($this, $fo) = @_;
+			print $fo join('', map { "\t$_\n" } @{$this->{'data'}});
+		}
+	},
+	{
+		# There should be no space as first character in the link
+		'name' => 'Links starting with space',
+		'data' => [],
+		'analyze' => sub ($$$) {
+			my ($this, $ln, $idx) = @_;
+			my $num = ($ln =~ s/<a [^<>]*>\s/$&/g);
+			if ($num) {
+				push @{$this->{'data'}}, "At line $idx, number of entries: $num";
+			}
+		},
+		'report' => sub ($$) {
+			my ($this, $fo) = @_;
+			print $fo join('', map { "\t$_\n" } @{$this->{'data'}});
+		}
+	},
+	{
+		# Template for adding new tests
+		'name' => 'Template',
+		'data' => [],
+		'analyze' => sub ($$$) {
+			my ($this, $ln, $idx) = @_;
+			if (0) {
+				push @{$this->{'data'}}, "Error at line $idx";
+			}
+		},
+		'report' => sub ($$) {
+			my ($this, $fo) = @_;
+			print $fo join('', map { "\t$_\n" } @{$this->{'data'}});
+		}
+	},
+);
+
+################################################################################
+# Main code
 
 my $fi;
 my $fo;
 
+# Open input/output files
 open($fi, '<', $ARGV[0]) or die "Failed to open input file '$ARGV[0]' for reading: $!";
 if ($ARGV[1]) {
 	open($fo, '>', $ARGV[1]) or die "Failed to open output file '$ARGV[1]' for writing: $!";
@@ -35,25 +150,22 @@ else {
 	$fo = \*STDOUT;
 }
 
-my %links = ();
-my %targets = ();
-
+# Read the input file line-by-line and call all test analyzers for each line
+my $idx = 0;
 while (my $ln = <$fi>) {
-	$ln =~ s/<[^<>]+ id="([^\"]+)"/$targets{$1} = 1; $&;/eg;
-	$ln =~ s/<a l:href="#([^\"]+)"/$links{$1} = 1; $&;/eg;
+	++$idx;
+	for my $test (@tests) {
+		$test->{'analyze'}->($test, $ln, $idx);
+	}
 }
 
-print $fo "Invalid links:\n";
-for my $i (sort(keys(%links))) {
-	print $fo "$i\n" if (!$targets{$i});
+# Now print collected test results
+for my $test (@tests) {
+	print $fo "Report from test '" . $test->{'name'} . "':\n";
+	$test->{'report'}->($test, $fo);
+	print $fo "\n";
 }
 
-print $fo "\n";
-
-print $fo "Orfan targets:\n";
-for my $i (sort(keys(%targets))) {
-	print $fo "$i\n" if (!$links{$i});
-}
-
+# Close files and exit
 close($fi);
 close($fo) if ($fo != \*STDOUT);
