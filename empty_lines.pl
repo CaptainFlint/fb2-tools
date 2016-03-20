9725348071a695d5a36253352cd454ef67e21823
Konstantin Vlasov <support@flint-inc.ru>
2016-03-21 02:53:07 +0300

    empty-lines: Various adaptations
    1. Source text file now provides not the full text, but just the list of lines that follow the newlines. Long lines are cut by 50 characters.
    2. Added epigraph, title, subtitle to the list of tags that should not contain empty-lines in the beginning.
    3. Improved no-starting-empty-lines fix procedure.
    4. Added information about duplicate matching of the source text lines.
    5. Added new mode: Output contains only matched lines instead of the full fb2.
    6. If empty line is present already at the required location, do not add a second one.

--- empty_lines.pl
+++ empty_lines.pl
@@ -5,7 +5,7 @@ use warnings;
 use utf8;
 
 if (scalar(@ARGV) < 3) {
-	print "Usage: $0 <reference-txt> <source-fb2> <target-fb2>\n";
+	print "Usage: $0 <newlines-txt> <source-fb2> { --out <target-fb2> | --marks <target-txt> } \n";
 	exit 1;
 }
 
@@ -19,41 +19,111 @@ sub readFile($) {
 }
 
 my $fo;
-my @txt = map { my $l = $_; $l =~ s/[ \x0d\x0a \d]//g; $l; } readFile($ARGV[0]);
+my @txt = map { my $l = $_; $l =~ s/[\x00-\x20\xa0\x{feff}]//g; $l; } readFile($ARGV[0]);
 my @fb2 = readFile($ARGV[1]);
 # Strip:
-# 1. all space characters (to compare sparse text),
-# 2. digits (because notes have different numbers),
-# 3. XML tags;
-# and translate XML entities.
-my @fb2s = map { my $l = $_; $l =~ s/[ \x0d\x0a \d]//g; $l =~ s/<[^<>]*>//g; $l =~ s/&lt;/</g; $l =~ s/&gt;/>/g; $l =~ s/&amp;/&/g; $l; } @fb2;
+# * all footnote references (they have different numbering),
+# * all XML tags;
+# * leading and trailing space characters,
+# then translate XML entities and cut off the first 50 characters of text.
+# After that prepare the text for non-strict comparison by removing:
+# * all control characters (may appear in newlines-only text files where footnotes refs were);
+# * all space characters (to compare sparse text),
+# * BOM marker (U+FEFF).
+my @fb2s = map {
+	my $l = $_;
+	$l =~ s|<a l:href="#_ftn\d+"><sup>\d+</sup></a>||g;
+	$l =~ s/<[^<>]*>//g;
+	$l =~ s/&lt;/</g;
+	$l =~ s/&gt;/>/g;
+	$l =~ s/&amp;/&/g;
+	$l =~ s/^\s+//;
+	$l =~ s/\s+$//;
+	$l = substr($l, 0, 50);
+	$l =~ s/[\x00-\x20\xa0\x{feff}]//g;
+	$l;
+} @fb2;
+# Array containing only non-empty lines from the source txt
+my @txts = ();
+# Matching from txts index to txt line number
+my @txts2txt = ();
 
-my $last_el = 0;
-my @els = ();
+# First, collect all requested entries
+my @els = ();   # for each index of the txt line => array of fb2 line indices
+my $idx = 0;
 for (my $i = 0; $i < scalar(@txt); ++$i) {
-	if (($txt[$i] eq '') && ($txt[$i + 1] ne '')) {
-		my $done = 0;
-		for (my $j = $last_el; $j < scalar(@fb2s); ++$j) {
-			if ($txt[$i + 1] eq $fb2s[$j]) {
-				push @els, $j;
-				$done = 1;
-				$last_el = $j;
-				last;
-			}
+	next if ($txt[$i] =~ m/^$/);
+	$txts2txt[$idx] = $i + 1;  # Adjust to 1-base
+	$txts[$idx] = $txt[$i];
+
+	$els[$idx] = [];
+	for (my $j = 0; $j < scalar(@fb2s); ++$j) {
+		if ($txts[$idx] eq $fb2s[$j]) {
+			push @{$els[$idx]}, $j;
 		}
-		if (!$done) {
-			print "FAILED to find line in fb2: txt index is " . ($i + 1) . "\n";
+	}
+	++$idx;
+}
+
+# Process the entries
+for (my $i = 0; $i < scalar(@els); ++$i) {
+	my $num_els = scalar(@{$els[$i]});
+	if ($num_els == 0) {
+		print "FAILED to find line in fb2: txt index is $txts2txt[$i]\n";
+		next;
+	}
+	if ($num_els > 1) {
+		# Multiple entries.
+		# Try to eliminate the wrong ones...
+		if (($i > 0) && (scalar(@{$els[$i - 1]}) == 1)) {
+			@{$els[$i]} = grep { $_ > $els[$i - 1][0] } @{$els[$i]};
+		}
+		if (($i < scalar(@els) - 1) && (scalar(@{$els[$i + 1]}) == 1)) {
+			@{$els[$i]} = grep { $_ < $els[$i + 1][0] } @{$els[$i]};
+		}
+		$num_els = scalar(@{$els[$i]});
+		if ($num_els > 1) {
+			print "DUPLICATE found in fb2 for txt line $txts2txt[$i]\n";
 		}
 	}
 }
 
-for my $idx (sort { $b <=> $a } @els) {
-	while ($fb2[--$idx] =~ m/<(stanza|poem|cite)>/) { }
+# If we need only marks, fill in the separate output array
+my @marks = ();
+
+# Finally, insert empty lines
+for (my $i = scalar(@els) - 1; $i >= 0; --$i) {
+	next if (scalar(@{$els[$i]}) != 1);
+	my $idx = $els[$i][0];
+	my @temp = ();
+	if ($ARGV[2] eq '--marks') {
+		unshift @temp, $fb2[$idx];
+	}
+	# If we are at the start of a cite, poem, title or epigraph, go backwards until we get out
+	while (1) {
+		--$idx;
+		# Continue while the current line contains an open tag from the list,
+		# and at the same time does not contain the same closing tag (that is, while we are
+		# actually inside the tag).
+		last if (($fb2[$idx] !~ m/<(stanza|poem|cite|epigraph|title|subtitle)( [^<>]*|)>/) || ($fb2[$idx] =~ m/<\/$1>/));
+		if ($ARGV[2] eq '--marks') {
+			unshift @temp, $fb2[$idx];
+		}
+	}
+	if ($fb2[$idx] =~ m/<empty-line\/>/) {
+		# Empty line already there, skipping
+		next;
+	}
 	++$idx;
-	my $sp = (($fb2[$idx] =~ m/^(\s+)/) ? $1 : '');
-	splice(@fb2, $idx, 0, "$sp<empty-line/>\n");
+	if ($ARGV[2] eq '--marks') {
+		unshift @marks, @temp, "\n";
+	}
+	else {
+		my $sp = (($fb2[$idx] =~ m/^(\s+)/) ? $1 : '');
+		splice(@fb2, $idx, 0, "$sp<empty-line/>\n");
+	}
 }
 
-open($fo, '>:encoding(UTF-8)', $ARGV[2]) or die "Failed to open $ARGV[2] for writing: $!";
-print $fo $_ foreach (@fb2);
+open($fo, '>:encoding(UTF-8)', $ARGV[3]) or die "Failed to open $ARGV[2] for writing: $!";
+print $fo $_ foreach (($ARGV[2] eq '--marks') ? @marks : @fb2);
 close($fo);
