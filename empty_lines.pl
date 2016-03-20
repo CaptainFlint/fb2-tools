a0fd2015cb378fb1ca302e223d73e6dfd0862551
Konstantin Vlasov <support@flint-inc.ru>
2016-03-20 18:37:41 +0300

    empty-lines: UTF-8 improvement, minor code cleanup

--- empty_lines.pl
+++ empty_lines.pl
@@ -19,14 +19,14 @@ sub readFile($) {
 }
 
 my $fo;
-my @txt = map { my $l = $_; $l =~ s/[ \x0d\x0a]| |\d//g; $l; } readFile($ARGV[0]);
+my @txt = map { my $l = $_; $l =~ s/[ \x0d\x0a \d]//g; $l; } readFile($ARGV[0]);
 my @fb2 = readFile($ARGV[1]);
 # Strip:
 # 1. all space characters (to compare sparse text),
 # 2. digits (because notes have different numbers),
 # 3. XML tags;
 # and translate XML entities.
-my @fb2s = map { my $l = $_; $l =~ s/[ \x0d\x0a]| |\d//g; $l =~ s/<[^<>]*>//g; $l =~ s/&lt;/</g; $l =~ s/&gt;/>/g; $l =~ s/&amp;/&/g; $l; } @fb2;
+my @fb2s = map { my $l = $_; $l =~ s/[ \x0d\x0a \d]//g; $l =~ s/<[^<>]*>//g; $l =~ s/&lt;/</g; $l =~ s/&gt;/>/g; $l =~ s/&amp;/&/g; $l; } @fb2;
 
 my $last_el = 0;
 my @els = ();
@@ -48,11 +48,10 @@ for (my $i = 0; $i < scalar(@txt); ++$i) {
 }
 
 for my $idx (sort { $b <=> $a } @els) {
-	my $idx2 = $idx;
-	while ($fb2[--$idx2] =~ m/<(stanza|poem|cite)>/) { }
-	++$idx2;
-	my $sp = (($fb2[$idx2] =~ m/^(\s+)/) ? $1 : '');
-	splice(@fb2, $idx2, 0, "$sp<empty-line/>\n");
+	while ($fb2[--$idx] =~ m/<(stanza|poem|cite)>/) { }
+	++$idx;
+	my $sp = (($fb2[$idx] =~ m/^(\s+)/) ? $1 : '');
+	splice(@fb2, $idx, 0, "$sp<empty-line/>\n");
 }
 
 open($fo, '>:encoding(UTF-8)', $ARGV[2]) or die "Failed to open $ARGV[2] for writing: $!";
